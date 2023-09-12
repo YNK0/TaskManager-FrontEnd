de9d28022ee432a1ce35504e9f2242ece61604bf
YNK0 <francisco.vargasmagana03@gmail.com>
2023-09-11 21:44:33 -0600

    Error title empty fixed

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import "./input.css";
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 function AddTaskForm({ onSubmit }) {
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     id: new Date().getTime(),
-    title: '',
-    description: '',
+    title: "",
+    description: "",
     status: "OPEN",
-  });
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [error, setError] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,16 +23,24 @@ function AddTaskForm({ onSubmit }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (formData.title.trim() === "") {
+      setError("Title is required");
+      return; 
+    }
     onSubmit(formData);
+    setError(null);
+    setFormData(initialFormData);
   };
 
   return (
+    <div>
     <form onSubmit={handleSubmit}>
       <input
         type="text"
         id="title"
         name="title"
         value={formData.title}
+        placeholder="Title"
         onChange={handleChange}
       />
       <input
@@ -37,9 +48,15 @@ function AddTaskForm({ onSubmit }) {
         name="description"
         value={formData.description}
         onChange={handleChange}
+        placeholder="Description"
       ></input>
-      <button type="submit" className="buttonSubmit">Add Task</button>
+      
+      <button type="submit" className="buttonSubmit">
+        Add Task
+      </button>
     </form>
+    {error && <p className="error">{error}</p>}
+    </div>
   );
 }
 
